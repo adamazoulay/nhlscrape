d05caabe843e8dd561c5e00018bbb8f34652eeef
Adam Azoulay <adamazoulay@live.ca>
2019-11-07 12:51:58 -0500

    Initial commit

---
+++ R/nhl_scrape.R
@@ -0,0 +1,76 @@
+api_url <- "https://statsapi.web.nhl.com/api/v1/"
+
+GetApiJson <- function(call) {
+  r <- httr::GET(paste(api_url, call))
+
+  # Make sure we have the correct data from the GET request
+  httr::stop_for_status(r)
+
+  # Get the text and parse it from JSON to a table
+  txt <- httr::content(r, "text")
+  json <- jsonlite::fromJSON(txt)
+  return(json)
+}
+
+GetTeams <- function() {
+  return(GetApiJson("teams"))
+}
+
+GetTeamId <- function(team_name) {
+  # Expect name to be either full name or abbreviation
+  team_list <- GetTeams()
+  team_names_full <- team_list$teams$name
+  team_names_abbr <- team_list$teams$abbreviation
+
+  team_id <- match(team_name, team_names_full)
+  if (!is.na(team_id)) {
+    return(team_id)
+  }
+
+  team_id <- match(team_name, team_names_abbr)
+  if (!is.na(team_id)) {
+    return(team_id)
+  }
+
+  stop("Could not find team with name: ", team_name)
+}
+
+GetGameIdNext <- function(team_id) {
+  request <- paste("teams/", team_id, "?expand=team.schedule.next", sep="")
+  r <- GetApiJson(request)
+  return(r$teams$nextGameSchedule$dates[[1]]$games[[1]]$gamePk)
+}
+
+GetGameIdRange <- function(team_id, date_range) {
+  request <- paste("schedule?teamId=", team_id, sep="")
+  request <- paste(request, "&startDate=", date_range[1], sep="")
+  request <- paste(request, "&endDate=", date_range[2], sep="")
+  r <- GetApiJson(request)
+
+  game_ids <- c()
+  for (game in r$dates$games) {
+    game_ids <- c(game_ids, game$gamePk)
+  }
+
+  return(game_ids)
+}
+
+GetGameLiveFeed <- function(game_id) {
+  request <- paste("game/", game_id, "/feed/live", sep="")
+  r <- GetApiJson(request)
+  plays <- r$liveData$plays$allPlays
+
+  # Gives times and other info about each play
+  # "eventIdx"            "eventId"             "period"              "periodType"          "ordinalNum"
+  # "periodTime"          "periodTimeRemaining" "dateTime"            "goals"
+  about <- plays$about
+
+  # Gives the location of all events, (0, 0) is center ice
+  # "x" "y"
+  coords <- plays$coordinates
+
+  # Gives the type and descriptions of each event
+  # "event"           "eventCode"       "eventTypeId"     "description"     "secondaryType"   "penaltySeverity"
+  # "penaltyMinutes"  "strength"        "gameWinningGoal" "emptyNet"
+  results <- plays$result
+}
