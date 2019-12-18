fb4eaafbfb741d417d4544e151bfea03c838f5f5
Adam Azoulay <adamazoulay@live.ca>
2019-12-18 16:06:16 -0500

    Added stat and db tests

--- tests/testthat/test.stats.R
+++ tests/testthat/test.stats.R
@@ -1,6 +1,10 @@
 context("Statistics equal")
 library(nhlscrape)
 
+
+player_id <- 8475166
+game_id <- 2019020001
+team_id <- 10
 test_that("Statistics for even strength and all situtations is the same", {
   expect_equal(GetPlayerStats(player_id, game_id, team_id)$CF[1], 28)
   expect_equal(GetPlayerStats(player_id, game_id, team_id)$CA[1], 12)
